42e90f4233ddd74e3ae2dd5b36813d3c0a052572
alf74486-lang <alf74486@gmail.com>
2026-03-17 13:17:54 +0300

    Update imgui_impl_ios.h

--- imgui_impl_ios.h
+++ imgui_impl_ios.h
@@ -5,3 +5,5 @@
 IMGUI_IMPL_API bool     ImGui_ImplIOS_Init(UIView* view);
 IMGUI_IMPL_API void     ImGui_ImplIOS_Shutdown();
 IMGUI_IMPL_API void     ImGui_ImplIOS_NewFrame(UIView* view);
+IMGUI_IMPL_API void ImGui_ImplIOS_HandleEvent(UITouch* touch, UIView* view, int eventType);
+// eventType: 0=Began, 1=Moved, 2=Ended
